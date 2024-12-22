1ba1bce98397e4525ced9390b2f1953a23c6340f
Miguel Ángel López Jiménez <mian.lopezjimenez@gmail.com>
2024-12-22 20:46:29 +0100

    chore: added space code

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="mt-8 mb-8 w-full md:w-[700px] flex flex-col gap-y-4">
       <h2 className="text-2xl md:text-3xl">
-        Create an account and start{" "}
+        Create an account and start&nbsp;
         <span className="font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
           Snip
         </span>
