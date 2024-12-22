5308c9aac3800ed89943a148c29f458d49f22577
Miguel Ángel López Jiménez <mian.lopezjimenez@gmail.com>
2024-12-22 23:34:56 +0100

    feature: added new GradientText component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,13 @@ import { PenTool, ChartNoAxesCombined } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Hero from "@/components/hero";
 import ShortURLForm from "@/components/short-url-form";
+import GradientText from "@/components/gradient-text";
 
 export default function Home() {
   return (
     <main className="container mx-auto flex flex-col items-center px-6 md:px-0 gap-y-3 md:gap-y-6">
       <h1 className="text-3xl md:text-5xl text-left md:text-center">
-        <span className="font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
-          Snip:
-        </span>{" "}
-        your links, your way.
+        <GradientText>Snip:</GradientText> your links, your way.
       </h1>
 
       <h2 className="mb-8 text-center w-fit text-lg md:text-2xl">
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,13 +1,12 @@
 import { Button } from "@/components/ui/button";
+import GradientText from "@/components/gradient-text";
 
 const Hero = () => {
   return (
     <section className="mt-8 mb-8 w-full md:w-[700px] flex flex-col gap-y-4">
       <h2 className="text-2xl md:text-3xl">
         Create an account and start&nbsp;
-        <span className="font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
-          Snip
-        </span>
+        <GradientText>Snip</GradientText>
         ping
       </h2>
 
