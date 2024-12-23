65113a33d056eac0fc3cf4b8c893d628dddbf9f4
Miguel Ángel López Jiménez <mian.lopezjimenez@gmail.com>
2024-12-23 23:46:51 +0100

    fix: commented unused code

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+      // const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
