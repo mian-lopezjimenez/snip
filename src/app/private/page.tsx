8e4f7e5ab6eea09e2d2d5c8987ffe6714bdd320e
Miguel Ángel López Jiménez <mian.lopezjimenez@gmail.com>
2024-12-28 23:44:12 +0100

    chore: test of geolocation vercel

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,3 +1,19 @@
+import Counters from "@/app/private/components/counters";
+import ClicksChart from "@/app/private/components/clicks-chart";
+import CountryChart from "@/app/private/components/country-chart";
+
 export default async function PrivatePage() {
-  return <p>Private Page</p>;
+  return (
+    <>
+      <h1 className="scroll-m-20 text-3xl md:text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Dashboard
+      </h1>
+      <Counters />
+
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <ClicksChart />
+        <CountryChart />
+      </section>
+    </>
+  );
 }
